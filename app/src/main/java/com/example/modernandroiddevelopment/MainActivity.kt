9f56f657f3d2030732f95830d4cf73ea9cabb9aa
yewon-yw <kimyewon0401@gmail.com>
2023-08-10 11:29:15 +0900

    앱 추기 구조 작성하기

--- app/src/main/java/com/example/modernandroiddevelopment/MainActivity.kt
+++ app/src/main/java/com/example/modernandroiddevelopment/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.modernandroiddevelopment
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.example.modernandroiddevelopment.databinding.ActivityMainBinding
+import com.example.modernandroiddevelopment.ui.view.FavoriteFragment
+import com.example.modernandroiddevelopment.ui.view.SearchFragment
+import com.example.modernandroiddevelopment.ui.view.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,50 +15,39 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(binding.root)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setContentView(binding.root)
 
-//        var counter = 100
-//        binding.textView.text = counter.toString()
-//
-//        binding.button.setOnClickListener{
-//            counter += 1
-//            binding.textView.text = counter.toString()
-//        }
-
-//        val myViewModel: MyViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-//        myViewModel.counter = 100
-//        binding.textView.text = myViewModel.counter.toString()
-//
-//        binding.button.setOnClickListener {
-//            myViewModel.counter += 1
-//            binding.textView.text = myViewModel.counter.toString()
-//        }
-
-        val myRepositoryImpl = MyRepositoryImpl(10)
-        val factory = MyViewModelFactory(10, myRepositoryImpl,this)
-//        val myViewModel = ViewModelProvider(this,factory).get(MyViewModel::class.java)
-        val myViewModel by viewModels<MyViewModel>() { factory }
-        // 팩토리를 적용해야하는 경우
-
-        binding.lifecycleOwner = this
-        binding.viewmodel = myViewModel
-
-        binding.textView.text = myViewModel.counter.toString()
-
-        binding.button.setOnClickListener {
-//            myViewModel.counter += 1
-//            binding.textView.text = myViewModel.counter.toString()
-//            myViewModel.saveState()
-//            myViewModel.liveCounter.value = myViewModel.liveCounter.value?.plus(1)
-            myViewModel.increaseCounter()
+        setupBottomNavigationView()
+        // 앱이 처음 실행됐을 경우에만 search fragment로 설정
+        if(savedInstanceState == null){
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
         }
-//        myViewModel.liveCounter.observe(this) { counter ->
-//            binding.textView.text = counter.toString()
-//        }
+    }
 
-//        myViewModel.modifiedCounter.observe(this) { counter ->
-//            binding.textView.text = counter.toString()
-//        }
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/modernandroiddevelopment/MyRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.modernandroiddevelopment
-
-import androidx.lifecycle.LiveData
-
-interface MyRepository {
-    fun getCounter(): LiveData<Int>
-    fun increaseCounter()
-}
