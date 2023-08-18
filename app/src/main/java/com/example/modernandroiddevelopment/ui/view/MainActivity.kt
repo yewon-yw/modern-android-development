fef10ab2e072139ffa4ec65cfdd133554aa66542
yewon-yw <kimyewon0401@gmail.com>
2023-08-18 16:11:28 +0900

    Navigation으로 UI의 화면 전환 구현하기

--- app/src/main/java/com/example/modernandroiddevelopment/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/modernandroiddevelopment/ui/view/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.modernandroiddevelopment.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.modernandroiddevelopment.R
 import com.example.modernandroiddevelopment.data.repository.BookSearchRepositoryImpl
 import com.example.modernandroiddevelopment.databinding.ActivityMainBinding
@@ -15,46 +21,69 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
     lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupBottomNavigationView()
-        // 앱이 처음 실행됐을 경우에만 search fragment로 설정
-        if(savedInstanceState == null){
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
-        }
+//        setupBottomNavigationView()
+//        앱이 처음 실행됐을 경우에만 search fragment로 설정
+//        if(savedInstanceState == null){
+//            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+//        }
+        setupJetpackNavigation()
 
         val bookSearchRepository = BookSearchRepositoryImpl()
         val factory = BookSearchViewModelProviderFactory(bookSearchRepository)
         bookSearchViewModel = ViewModelProvider(this, factory)[BookSearchViewModel::class.java]
     }
 
-    private fun setupBottomNavigationView() {
-        binding.bottomNavigationView.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.fragment_search -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SearchFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_favorite -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, FavoriteFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_settings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SettingsFragment())
-                        .commit()
-                    true
-                }
-                else -> false
-            }
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.booksearch_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
 
-        }
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragment_search, R.id.fragment_favorite, R.id.fragment_settings
+            )
+        )
+        // 현재 navigation 계층 구조에 따라서 search Fragment가 top level destination으로 지정이 됨
+        // setupActionBarWithNavController로 navController, appBarConfiguration을 연결시켜줌
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+//    private fun setupBottomNavigationView() {
+//        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+//            when(item.itemId) {
+//                R.id.fragment_search -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SearchFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_favorite -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, FavoriteFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_settings -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SettingsFragment())
+//                        .commit()
+//                    true
+//                }
+//                else -> false
+//            }
+//
+//        }
+//    }
 }
