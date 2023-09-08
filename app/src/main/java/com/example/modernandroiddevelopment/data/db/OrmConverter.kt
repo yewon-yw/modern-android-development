8eb43c64ab76c10ea529b263a0c05045de266575
yewon-yw <kimyewon0401@gmail.com>
2023-09-08 16:04:09 +0900

    검색 결과 저장을 위한 Room DB 구현하기

---
+++ app/src/main/java/com/example/modernandroiddevelopment/data/db/OrmConverter.kt
@@ -0,0 +1,13 @@
+package com.example.modernandroiddevelopment.data.db
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class OrmConverter {
+    @TypeConverter
+    fun fromList(value: List<String>) = Json.encodeToString(value)
+
+    @TypeConverter
+    fun toList(value: String) = Json.decodeFromString<List<String>>(value)
+}
